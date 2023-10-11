7f8c67f46e0dbb38ac4927590a23d8e7a5fd7ea7
ChristianBernhard <christian.bernhard97@web.de>
2023-10-11 15:41:51 +0200

    refactored 1 und 2 qubit gates modular

--- quantum/tn.py
+++ quantum/tn.py
@@ -19,10 +19,11 @@ class TNtemplate:
     def iterate_circ(self):
         if (self.circ is None): raise Exception("circ is None")
         for gate in self.circ.gates:
-            print("Tensornetwork: ", self.tensor_network)
+            print("Tensornetwork before Gate: ", self.tensor_network)
             getattr(self, gate.name)(gate)
             print("Gate: ", gate.name, " applied!")
-            print("Tensornetwork: ", self.tensor_network)
+            print("Tensornetwork after Gate: ", self.tensor_network)
+
 
     def simulate(self):
         # Iterate Circuit
@@ -30,53 +31,58 @@ class TNtemplate:
 
     def x(self, gate: Gate):
         x_gate = np.array([[0, 1], [1, 0]])
-        target_tensor = self.tensor_network[gate.target]
-        modified_tensor = np.einsum('abc,da->dbc', target_tensor, x_gate)
-        self.tensor_network[gate.target] = modified_tensor
+        self.apply_single_qubit_gate(gate, x_gate)
 
-    def y(self, gate):
+    def y(self, gate: Gate):
         y_gate = np.array([[0, -1j], [1j, 0]])
-        target_tensor = self.tensor_network[gate.target]
-        modified_tensor = np.einsum('abc,da->dbc', target_tensor, y_gate)
-        self.tensor_network[gate.target] = modified_tensor
+        self.apply_single_qubit_gate(gate, y_gate)
 
-    def z(self, gate):
+    def z(self, gate: Gate):
         z_gate = np.array([[1, 0], [0, -1]])
+        self.apply_single_qubit_gate(gate, z_gate)
+
+    def h(self, gate: Gate):
+        h_gate = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+        self.apply_single_qubit_gate(gate, h_gate)
+
+    def apply_single_qubit_gate(self, gate: Gate, u_gate: np.ndarray):
         target_tensor = self.tensor_network[gate.target]
-        modified_tensor = np.einsum('abc,da->dbc', target_tensor, z_gate)
+        modified_tensor = np.einsum('abc,da->dbc', target_tensor, u_gate)
         self.tensor_network[gate.target] = modified_tensor
 
-    def h(self, gate):
-        h_gate = np.array([[1, 1], [1, -1]])
-        h_gate = h_gate / np.sqrt(2)
-        target_tensor = self.tensor_network[gate.target]
-        modified_tensor = np.einsum('abc,da->dbc', target_tensor, h_gate)
-        self.tensor_network[gate.target] = modified_tensor
 
-    def cx(self, gate):
-        ## Setup
-        control_tensor = self.tensor_network[gate.control]
-        target_tensor = self.tensor_network[gate.target]
-        U_gate = np.array([[[[1, 0], [0, 0]], [[0, 1], [0, 0]]], [[[0, 0], [0, 1]], [[0, 0], [1, 0]]]])
+    def apply_two_qubit_gate(self, gate: Gate, u_gate: np.ndarray):
+        # Decide the order of tensors and rearrange unitary if needed
+        ## reversed order
+        if gate.control > gate.target:
+            control_tensor = self.tensor_network[gate.target]
+            target_tensor = self.tensor_network[gate.control]
+            u_gate = np.swapaxes(np.swapaxes(u_gate, 0, 1), 2, 3)  # Swap control and target in the unitary
+            print("U-Gate: ", u_gate)
+            print(u_gate.shape)
+        else:
+            control_tensor = self.tensor_network[gate.control]
+            target_tensor = self.tensor_network[gate.target]
 
-        ## Contract M(n) & M(n+1) to T
+
+        # Two Qubit Gate Procedure
         print("Control: ", control_tensor)
         print("Target: ", target_tensor)
         T = np.einsum('abc,dce->adbe', control_tensor, target_tensor)
         print("Contracted 4 dim T: ", T)
 
         ## Contract U and T to T'
-        T_strich = np.einsum('abcd,cdef->abef', U_gate, T)
+        T_strich = np.einsum('abcd,cdef->abef', u_gate, T)
         print("Contracted 4 dim UT aka. T': ", T_strich)
 
         ## Apply SVD to obtain U, S, V^T
         E, F, A, B = T_strich.shape
-        T_strich_reshaped = T_strich.reshape((E * A, F * B))
-        print(T_strich_reshaped)
+        print("PRINT TSTRICH: ", T_strich)
+        T_strich_reshaped = np.concatenate((np.concatenate((T_strich[0][0], T_strich[0][1]), axis=1),
+                            np.concatenate((T_strich[1][0], T_strich[1][1]), axis=1)), axis=0)
         U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
-        V_dagger = np.conj(V_dagger).T ## Check it this is needed
 
-        chi_min = min(self.chi, len(S)) ## error checking for chi
+        chi_min = min(self.chi, len(S)) ## error checking for chi TODO: Make this more sophisticated
         S_diag = np.diag(S[:self.chi])
         U = U[:, :chi_min]
         V_dagger = V_dagger[:chi_min, :]
@@ -84,107 +90,49 @@ class TNtemplate:
         print("Truncation to: ", chi_min, " from ", len(S), 
               " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
 
-        # Multiply singular values into U and V
-        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
-        M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
+        # M = US, M' = V
+        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))
+        M1_strich = np.array(np.hsplit(V_dagger, 2))
 
         print("M_strich: ", M_strich)
         print("M1_strich: ", M1_strich)
 
-        self.tensor_network[gate.control] = M_strich
-        self.tensor_network[gate.target] = M1_strich
+        if gate.control > gate.target:
+            self.tensor_network[gate.target], self.tensor_network[gate.control] = M_strich, M1_strich
+        else:
+            self.tensor_network[gate.control], self.tensor_network[gate.target] = M_strich, M1_strich
 
         print("Result: ", M_strich[1] @ M1_strich[1])
 
+
+    def cx(self, gate):
+        U_cx = np.array([
+          [1, 0, 0, 0],
+          [0, 1, 0, 0],
+          [0, 0, 0, 1],
+          [0, 0, 1, 0]
+      ]).reshape(2, 2, 2, 2)
+        self.apply_two_qubit_gate(gate, U_cx)
+
+
     def cz(self, gate):
-        ## Setup
-        control_tensor = self.tensor_network[gate.control]
-        target_tensor = self.tensor_network[gate.target]
-        U_gate = np.array([[[[1, 0], [0, 0]], [[0, 0], [0, 1]]],
-                 [[[0, 1], [0, 0]], [[0, 0], [1, 0]]],
-                 [[[0, 0], [1, 0]], [[1, 0], [0, 0]]],
-                 [[[0, 0], [0, -1]], [[0, -1], [0, 0]]]])
-
-
-        ## Contract M(n) & M(n+1) to T
-        print("Control: ", control_tensor)
-        print("Target: ", target_tensor)
-        T = np.einsum('abc,dce->adbe', control_tensor, target_tensor)
-        print("Contracted 4 dim T: ", T)
-
-        ## Contract U and T to T'
-        T_strich = np.einsum('abcd,cdef->abef', U_gate, T)
-        print("Contracted 4 dim UT aka. T': ", T_strich)
-
-        ## Apply SVD to obtain U, S, V^T
-        E, F, A, B = T_strich.shape
-        T_strich_reshaped = T_strich.reshape((E * A, F * B))
-        print(T_strich_reshaped)
-        U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
-        V_dagger = np.conj(V_dagger).T ## Check it this is needed
-
-        chi_min = min(self.chi, len(S)) ## error checking for chi
-        S_diag = np.diag(S[:self.chi])
-        U = U[:, :chi_min]
-        V_dagger = V_dagger[:chi_min, :]
-
-        print("Truncation to: ", chi_min, " from ", len(S), 
-              " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
-
-        # Multiply singular values into U and V
-        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
-        M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
-
-        print("M_strich: ", M_strich)
-        print("M1_strich: ", M1_strich)
-
-        self.tensor_network[gate.control] = M_strich
-        self.tensor_network[gate.target] = M1_strich
+      U_cz = np.array([
+          [1, 0, 0, 0],
+          [0, 1, 0, 0],
+          [0, 0, 1, 0],
+          [0, 0, 0, -1]
+      ]).reshape(2, 2, 2, 2)
+      self.apply_two_qubit_gate(gate, U_cz)
 
     def cy(self, gate):
-        ## Setup
-        control_tensor = self.tensor_network[gate.control]
-        target_tensor = self.tensor_network[gate.target]
-        U_gate = np.array([[[[1, 0], [0, 0]], [[0, 0], [0, 1]]],
-                 [[[0, 1], [0, 0]], [[0, 0], [1, 0]]],
-                 [[[0, 0], [0, -1j]], [[0, 1j], [0, 0]]],
-                 [[[0, 0], [1j, 0]], [[-1j, 0], [0, 0]]]])
+        U_cy = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, -1j],
+            [0, 0, 1j, 0]
+        ]).reshape(2, 2, 2, 2)
+        self.apply_two_qubit_gate(gate, U_cy)
 
-
-        ## Contract M(n) & M(n+1) to T
-        print("Control: ", control_tensor)
-        print("Target: ", target_tensor)
-        T = np.einsum('abc,dce->adbe', control_tensor, target_tensor)
-        print("Contracted 4 dim T: ", T)
-
-        ## Contract U and T to T'
-        T_strich = np.einsum('abcd,cdef->abef', U_gate, T)
-        print("Contracted 4 dim UT aka. T': ", T_strich)
-
-        ## Apply SVD to obtain U, S, V^T
-        E, F, A, B = T_strich.shape
-        T_strich_reshaped = T_strich.reshape((E * A, F * B))
-        print(T_strich_reshaped)
-        U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
-        V_dagger = np.conj(V_dagger).T ## Check it this is needed
-
-        chi_min = min(self.chi, len(S)) ## error checking for chi
-        S_diag = np.diag(S[:self.chi])
-        U = U[:, :chi_min]
-        V_dagger = V_dagger[:chi_min, :]
-
-        print("Truncation to: ", chi_min, " from ", len(S), 
-              " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
-
-        # Multiply singular values into U and V
-        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
-        M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
-
-        print("M_strich: ", M_strich)
-        print("M1_strich: ", M1_strich)
-
-        self.tensor_network[gate.control] = M_strich
-        self.tensor_network[gate.target] = M1_strich
 
     def rx(self, gate):
         angle = gate.param
