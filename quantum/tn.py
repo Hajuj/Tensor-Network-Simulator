3af1edf3aa5ec21bbe94c505d744ac91b1c27a1c
ChristianBernhard <christian.bernhard97@web.de>
2023-10-11 09:39:12 +0200

    added CZ and CY

--- quantum/tn.py
+++ quantum/tn.py
@@ -14,7 +14,7 @@ class TNtemplate:
             zero_tensor = np.zeros((2, 1, 1), dtype=complex)
             zero_tensor[0, 0, 0] = 1
             self.tensor_network.append(zero_tensor)
-            self.chi = 30
+            self.chi = 2
 
     def iterate_circ(self):
         if (self.circ is None): raise Exception("circ is None")
@@ -54,28 +54,6 @@ class TNtemplate:
         self.tensor_network[gate.target] = modified_tensor
 
     def cx(self, gate):
-        # Setup
-        # zero = np.array([[1, 0], [0, 0]])
-        # one = np.array([[0, 0], [0, 1]])
-        # x_gate = np.array([[0, 1], [1, 0]])
-        # identity = np.identity(2)
-
-        # identities = [identity for _ in range(self.circ.numQubits)]
-        # identities_x = [identity for _ in range(self.circ.numQubits)]
-
-        # identities[gate.control] = zero
-        # identities_x[gate.control] = one
-        # identities_x[gate.target] = x_gate
-
-        # unitary_1 = unitary_2 = 1
-        # for iden, iden_x in zip(identities, identities_x):
-        #     unitary_1 = np.kron(iden, unitary_1)
-        #     unitary_2 = np.kron(iden_x, unitary_2)
-
-        # Apply
-        # unitary = unitary_1 + unitary_2
-        # self.tensor_network = np.matmul(unitary, self.tensor_network)
-
         ## Setup
         control_tensor = self.tensor_network[gate.control]
         target_tensor = self.tensor_network[gate.target]
@@ -96,17 +74,18 @@ class TNtemplate:
         T_strich_reshaped = T_strich.reshape((E * A, F * B))
         print(T_strich_reshaped)
         U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
-        # V = np.conj(V_dagger).T ## check if it works
-        print("S: ", S)
-        # S_diag = np.diag(S[:self.chi])  # chi is the bond dimension you want to keep
+        V_dagger = np.conj(V_dagger).T ## Check it this is needed
 
-        # ## check truncation
-        # U_truncated = U[:, self.chi]
-        # S_truncated = S[self.chi]
-        # V_truncated = V_dagger[self.chi, :]
+        chi_min = min(self.chi, len(S)) ## error checking for chi
+        S_diag = np.diag(S[:self.chi])
+        U = U[:, :chi_min]
+        V_dagger = V_dagger[:chi_min, :]
+
+        print("Truncation to: ", chi_min, " from ", len(S), 
+              " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
 
         # Multiply singular values into U and V
-        M_strich = np.array(np.vsplit(np.matmul(U, np.diag(S)), 2))  # vsplit
+        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
         M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
 
         print("M_strich: ", M_strich)
@@ -118,50 +97,94 @@ class TNtemplate:
         print("Result: ", M_strich[1] @ M1_strich[1])
 
     def cz(self, gate):
-        # Setup
-        zero = np.array([[1, 0], [0, 0]])
-        one = np.array([[0, 0], [0, 1]])
-        z_gate = np.array([[0, 1], [1, 0]])
-        identity = np.identity(2)
+        ## Setup
+        control_tensor = self.tensor_network[gate.control]
+        target_tensor = self.tensor_network[gate.target]
+        U_gate = np.array([[[[1, 0], [0, 0]], [[0, 0], [0, 1]]],
+                 [[[0, 1], [0, 0]], [[0, 0], [1, 0]]],
+                 [[[0, 0], [1, 0]], [[1, 0], [0, 0]]],
+                 [[[0, 0], [0, -1]], [[0, -1], [0, 0]]]])
 
-        identities = [identity for _ in range(self.circ.numQubits)]
-        identities_z = [identity for _ in range(self.circ.numQubits)]
 
-        identities[gate.control] = zero
-        identities_z[gate.control] = one
-        identities_z[gate.target] = z_gate
+        ## Contract M(n) & M(n+1) to T
+        print("Control: ", control_tensor)
+        print("Target: ", target_tensor)
+        T = np.einsum('abc,dce->adbe', control_tensor, target_tensor)
+        print("Contracted 4 dim T: ", T)
 
-        unitary_1 = unitary_2 = 1
-        for iden, iden_z in zip(identities, identities_z):
-            unitary_1 = np.kron(iden, unitary_1)
-            unitary_2 = np.kron(iden_z, unitary_2)
+        ## Contract U and T to T'
+        T_strich = np.einsum('abcd,cdef->abef', U_gate, T)
+        print("Contracted 4 dim UT aka. T': ", T_strich)
 
-        # Apply
-        unitary = unitary_1 + unitary_2
-        self.tensor_network = np.matmul(unitary, self.tensor_network)
+        ## Apply SVD to obtain U, S, V^T
+        E, F, A, B = T_strich.shape
+        T_strich_reshaped = T_strich.reshape((E * A, F * B))
+        print(T_strich_reshaped)
+        U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
+        V_dagger = np.conj(V_dagger).T ## Check it this is needed
+
+        chi_min = min(self.chi, len(S)) ## error checking for chi
+        S_diag = np.diag(S[:self.chi])
+        U = U[:, :chi_min]
+        V_dagger = V_dagger[:chi_min, :]
+
+        print("Truncation to: ", chi_min, " from ", len(S), 
+              " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
+
+        # Multiply singular values into U and V
+        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
+        M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
+
+        print("M_strich: ", M_strich)
+        print("M1_strich: ", M1_strich)
+
+        self.tensor_network[gate.control] = M_strich
+        self.tensor_network[gate.target] = M1_strich
 
     def cy(self, gate):
-        # Setup
-        zero = np.array([[1, 0], [0, 0]])
-        one = np.array([[0, 0], [0, 1]])
-        y_gate = np.array([[0, 1], [1, 0]])
-        identity = np.identity(2)
+        ## Setup
+        control_tensor = self.tensor_network[gate.control]
+        target_tensor = self.tensor_network[gate.target]
+        U_gate = np.array([[[[1, 0], [0, 0]], [[0, 0], [0, 1]]],
+                 [[[0, 1], [0, 0]], [[0, 0], [1, 0]]],
+                 [[[0, 0], [0, -1j]], [[0, 1j], [0, 0]]],
+                 [[[0, 0], [1j, 0]], [[-1j, 0], [0, 0]]]])
 
-        identities = [identity for _ in range(self.circ.numQubits)]
-        identities_y = [identity for _ in range(self.circ.numQubits)]
 
-        identities[gate.control] = zero
-        identities_y[gate.control] = one
-        identities_y[gate.target] = y_gate
+        ## Contract M(n) & M(n+1) to T
+        print("Control: ", control_tensor)
+        print("Target: ", target_tensor)
+        T = np.einsum('abc,dce->adbe', control_tensor, target_tensor)
+        print("Contracted 4 dim T: ", T)
 
-        unitary_1 = unitary_2 = 1
-        for iden, iden_y in zip(identities, identities_y):
-            unitary_1 = np.kron(iden, unitary_1)
-            unitary_2 = np.kron(iden_y, unitary_2)
+        ## Contract U and T to T'
+        T_strich = np.einsum('abcd,cdef->abef', U_gate, T)
+        print("Contracted 4 dim UT aka. T': ", T_strich)
 
-        # Apply
-        unitary = unitary_1 + unitary_2
-        self.tensor_network = np.matmul(unitary, self.tensor_network)
+        ## Apply SVD to obtain U, S, V^T
+        E, F, A, B = T_strich.shape
+        T_strich_reshaped = T_strich.reshape((E * A, F * B))
+        print(T_strich_reshaped)
+        U, S, V_dagger = np.linalg.svd(T_strich_reshaped, full_matrices=False)
+        V_dagger = np.conj(V_dagger).T ## Check it this is needed
+
+        chi_min = min(self.chi, len(S)) ## error checking for chi
+        S_diag = np.diag(S[:self.chi])
+        U = U[:, :chi_min]
+        V_dagger = V_dagger[:chi_min, :]
+
+        print("Truncation to: ", chi_min, " from ", len(S), 
+              " and S had shape: ", S.shape, " while S_diag trunc. has: ", S_diag.shape)
+
+        # Multiply singular values into U and V
+        M_strich = np.array(np.vsplit(np.matmul(U, S_diag), 2))  # vsplit
+        M1_strich = np.array(np.hsplit(V_dagger, 2))  # hsplit
+
+        print("M_strich: ", M_strich)
+        print("M1_strich: ", M1_strich)
+
+        self.tensor_network[gate.control] = M_strich
+        self.tensor_network[gate.target] = M1_strich
 
     def rx(self, gate):
         angle = gate.param
@@ -216,6 +239,6 @@ np.set_printoptions(suppress=True)
 
 if __name__ == "__main__":
     # c = parseQCP("code/QCPBench/small/test_n1.qcp")
-    c = parseQCP("../QCPBench/small/test_n1.qcp")
+    c = parseQCP("QCPBench/small/test_n1.qcp")
     simulator = TNtemplate(c)
     simulator.simulate()
